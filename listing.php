1358e68360ad8a64150d86731f13b84c57e49da1
Duskraptor <jeffdatahan@gmail.com>
2018-12-01 21:42:33 -0700

    Added search in Browse
    not done

--- listing.php
+++ listing.php
@@ -104,6 +104,20 @@
                         <div class="detail-filter-text">
                             <p><?php echo get_num_restaurants() ?> Results For <span>Restaurant</span></p>
                         </div>
+                        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Search</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+    <form action="search.php" method="GET">
+        <input type="text" placeholder="Find Users" class="btn-group1" name="search" />
+        <input type="submit" value="Search" />
+    </form>
+</body>
+</html>
                     </div>
                     <div class="col-md-8 featured-responsive">
                         <div class="detail-filter">
---
+++ search.php
@@ -0,0 +1,205 @@
+<?php include('functions.php');?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Colorlib">
+    <meta name="description" content="#">
+    <meta name="keywords" content="#">
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="#">
+    <!-- Page Title -->
+    <title>Listing &amp; Directory Website Template</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700,900" rel="stylesheet">
+    <!-- Simple line Icon -->
+    <link rel="stylesheet" href="css/simple-line-icons.css">
+    <!-- Themify Icon -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- Hover Effects -->
+    <link rel="stylesheet" href="css/set1.css">
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<!--============================= HEADER =============================-->
+<div class="dark-bg sticky-top">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="index.php">Calgary Reviews</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-menu"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="listing.php">Browse</a>
+                            </li>
+                            <?php if (isset($_SESSION['user'])) : ?>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown"
+                                       aria-haspopup="true" aria-expanded="false">
+                                        <?php echo $_SESSION['user']['username']; ?>
+                                        <small>
+                                            <i style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>
+                                                )</i>
+                                        </small>
+                                        <span class="icon-arrow-down"></span>
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                        <a class="dropdown-item" href="profile.php">Profile</a>
+                                        <a class="dropdown-item" href="#">Lists</a>
+                                        <a class="dropdown-item" href="viewuserphotos.php?username=<?php echo $_SESSION['user']['username'] ?>">Photos</a>
+                                    </div>
+                                </li>
+                                <?php if (isAdmin()) : ?>
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="admin/create_user.php" style="color: red;">Create
+                                            User</a>
+                                    </li>
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="admin/viewpending.php" style="color: red;">View
+                                            Pending</a>
+                                    </li>
+                                <?php endif ?>
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="index.php?logout='1'" style="color: red;">Logout</a>
+                                </li>
+                            <?php else: ?>
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="register.php">Register</a>
+                                </li>
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="login.php">Login</a>
+                                </li>
+                            <?php endif ?>
+                            <li><a href="addlisting.php" class="btn btn-outline-light top-btn"><span class="ti-plus"></span> Add
+                                    Listing</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+<!--//END HEADER -->
+<!--============================= DETAIL =============================-->
+ 
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+ <html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+     <title>Search results</title>
+     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+     <link rel="stylesheet" type="text/css" href="style.css"/>
+ </head>
+ <body>
+ <?php
+ 
+     $query = $_GET['search']; 
+     // gets value sent over search form
+      
+      
+     if(strlen($query) >=1){ // if query length is more or equal minimum length then
+          
+         $query = htmlspecialchars($query); 
+         // changes characters used in html to their equivalents, for example: < to &gt;
+          
+         $query = e($query);
+         // makes sure nobody uses SQL injection
+          
+         $results = "SELECT username,fname,lname FROM user WHERE username LIKE '%" . $query . "%' OR fname LIKE '%" . $query ."%' OR lname LIKE '%" . $query ."%'" ;
+             
+         $raw_results = mysqli_query($db, $results);
+              
+
+          
+         // '%$query%' is what we're looking for, % means anything, for example if $query is Hello
+         // it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
+         // or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
+         echo "<div>SEARCH RESULTS: '$query'</div>";
+         
+
+         if(mysqli_num_rows($raw_results) > 0){ // if one or more rows are returned do following
+              
+             while($results2 = mysqli_fetch_array($raw_results)){
+             // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
+                 
+                 //echo "<p><h3>".$results2['username']."</h3>".$results2['fname']."</p>";
+                 // posts results gotten from database(title and text) you can also show id ($results['id'])
+                 $user_id = $results2['username'];
+                 $href = "showuser.php?username=$user_id";
+                 echo <<< EOT
+
+                
+                 <div class="customer-img">
+                     <a href=$href> <p>$user_id</p> </a> 
+                 
+EOT;
+                if ($results2['fname'] == NULL){
+                    echo "First Name: NULL";
+                }
+                else{
+                    echo "First Name: "."".$results2['fname'];
+                }
+                echo "<div>";
+                if ($results2['lname'] == NULL){
+                    echo "Last Name: NULL";
+                }
+                else{
+                    echo "Last Name: "."".$results2['lname'];
+                }
+                echo "<hr>";
+             }
+              
+         }
+         else{ // if there is no matching rows do following
+             echo "There are no results";
+         }
+          
+     }
+     else{ // if query length is less than minimum
+         echo "Please enter at least one letter";
+     }
+ ?>
+ </body>
+ </html>
+
+<!--//END DETAIL -->
+<!--============================= FOOTER =============================-->
+<footer class="main-block dark-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="copyright">
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    <p>Copyright &copy; 2018 Listing. All rights reserved | This template is made with <i
+                                class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com"
+                                                                               target="_blank">Colorlib</a></p>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--//END FOOTER -->
+
+
+<!-- jQuery, Bootstrap JS. -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
